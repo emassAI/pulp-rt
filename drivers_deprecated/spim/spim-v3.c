fe9bf30c9255162ba3a3a2e62aae1b3b05811143
Sergio Ruocco <sergio.ruocco@gmail.com>
2025-02-09 19:05:39 +0000

    drivers_deprecated/spim/spim-v3.c: copy here the clarifying comments added in the branch 'emass-dev' now malfunctioning.

--- drivers_deprecated/spim/spim-v3.c
+++ drivers_deprecated/spim/spim-v3.c
@@ -52,7 +52,7 @@ static inline int __rt_spim_get_byte_align(int wordsize, int big_endian)
 
 rt_spim_t *rt_spim_open(char *dev_name, rt_spim_conf_t *conf, rt_event_t *event)
 {
-  int irq = rt_irq_disable();
+  int irq = rt_irq_disable(); // critical section
 
   rt_spim_conf_t def_conf;
 
@@ -81,13 +81,13 @@ rt_spim_t *rt_spim_open(char *dev_name, rt_spim_conf_t *conf, rt_event_t *event)
 
   if (channel == -1) goto error;
 
-  rt_spim_t *spim = rt_alloc(RT_ALLOC_FC_DATA, sizeof(rt_spim_t));
+  rt_spim_t *spim = rt_alloc(RT_ALLOC_FC_DATA, sizeof(rt_spim_t)); // note: ALLOC_FC_DATA 
   if (spim == NULL) goto error;
 
   spim->channel = channel;
 
-  spim->wordsize = conf->wordsize;
-  spim->big_endian = conf->big_endian;
+  spim->wordsize = conf->wordsize; // default (below) is WORDSIZE_8 !
+  spim->big_endian = conf->big_endian; // default (below) is 0
   spim->polarity = conf->polarity;
   spim->phase = conf->phase;
   spim->max_baudrate = conf->max_baudrate;
@@ -103,7 +103,7 @@ rt_spim_t *rt_spim_open(char *dev_name, rt_spim_conf_t *conf, rt_event_t *event)
 
   __rt_spim_open_count[id]++;
 
-  if (__rt_spim_open_count[id] == 1)
+  if (__rt_spim_open_count[id] == 1) // if this is the first open, then init more things!
   {
     plp_udma_cg_set(plp_udma_cg_get() | (1<<channel));
     __rt_udma_callback[channel] = __rt_spim_handle_eot;
@@ -111,7 +111,7 @@ rt_spim_t *rt_spim_open(char *dev_name, rt_spim_conf_t *conf, rt_event_t *event)
     soc_eu_fcEventMask_setEvent(ARCHI_SOC_EVENT_PERIPH_EVT_BASE(channel) + ARCHI_UDMA_SPIM_EOT_EVT);
   }
 
-  rt_irq_restore(irq);
+  rt_irq_restore(irq); // end of critical section
 
   return spim;
 
@@ -199,51 +199,74 @@ static inline __attribute__((always_inline)) int __rt_spim_periph_push(rt_periph
 
 void __rt_spim_send_async(rt_spim_t *handle, void *data, size_t len, int qspi, rt_spim_cs_e cs_mode, rt_event_t *event)
 {
-  int spim_id = __rt_spim_id(handle->channel);
-  int periph_id = handle->channel;
-  int periph_base = hal_udma_periph_base(periph_id);
-  int cmd_base = periph_base + ARCHI_SPIM_CMD_OFFSET;
-  int channel_base = periph_base + UDMA_CHANNEL_TX_OFFSET;
-  int buffer_size = len/8;
-  rt_periph_copy_t *copy = &event->implem.copy;
-  rt_spim_cmd_t *cmd = (rt_spim_cmd_t *)copy->periph_data;
-  rt_periph_spim_t *periph = &__rt_spim_periph[spim_id];
+  int spim_id = __rt_spim_id(handle->channel); // spim_id computed / based on channel ?
+  int periph_id = handle->channel; // is the periph_id == channel (tx) ?
+  int periph_base = hal_udma_periph_base(periph_id); // udma peripheral base ?
+  int cmd_base = periph_base + ARCHI_SPIM_CMD_OFFSET; // SPI commands: sequence of 32-bit microcode ops, fetched with DMA
+  int channel_base = periph_base + UDMA_CHANNEL_TX_OFFSET; // tx channel base
+  int buffer_size = len/8; // # of 8-bit bytes of data to send from 'data' buffer
+  rt_periph_copy_t *copy = &event->implem.copy; // copy the event ?
+  rt_spim_cmd_t *cmd = (rt_spim_cmd_t *)copy->periph_data; // copy the cmd, that is, the address of spi microcode array ?
+  rt_periph_spim_t *periph = &__rt_spim_periph[spim_id]; // which spi device are we using ?
 
   rt_trace(RT_TRACE_SPIM, "[SPIM] Send bitstream (handle: %p, buffer: %p, len: 0x%x, qspi: %d, keep_cs: %d, event: %p)\n", handle, data, len, qspi, cs_mode, event);
 
-  int irq = rt_irq_disable();
+  int irq = rt_irq_disable(); // critical section
 
-  copy->event = event;
+  copy->event = event; // copy event (again) ?
 
-  cmd->cmd[0] = handle->cfg;
-  cmd->cmd[1] = SPI_CMD_SOT(handle->cs);
+  cmd->cmd[0] = handle->cfg;  // copy configuration for spi microcode 
+  cmd->cmd[1] = SPI_CMD_SOT(handle->cs); // copy configuration for spi microcode 
+
   /*
-    Not documented in datasheet 
-     SPI_CMD_TX_DATA(words, wordstrans, bitsword, qpi, lsbfirst)
-       SPI_CMD_TX_DATA_ID << 28 | qpi << 27 | lsbfirst << 26 | wordstrans << 21 | (bitsword - 1) << 16 | (words-1)
-        words: number of 32-bit word
-        wordstrans: 0 = 1 bit, 1 = 2 bits, 2 = 4 bits
-        bitsword - 1 = 32 - 1 (0x11111)
-        qpi: 0 = spi, 1 = qspi
+	Not documented in datasheet
+
+	SPI_CMD_TX_DATA(words, wordspertrans, bitsperword, qpi, lsbfirst)
+
+	SPI_CMD_TX_DATA_ID << 28 |
+	qpi << 27 |
+	lsbfirst << 26 |
+	wordspertrans << 21 |
+	(bitsperword - 1) << 16 | (words-1) // what is 'words'? why words-1? CHECK!
+
+	wordspertrans: how many x-bit sized words are transmitted in a burst
+		4 = 4_WORD_PER_TRANSF; 1 = 2_WORD_PER_TRANSF; 0 = 1_WORD_PER_TRANSF
+
+	bitsperword: how many bits wide is a word transmitted over MOSI. In a 5-bit bitfield
+		32 bit wide words are encoded as 0x11111 = 31 (+1) = 32;
+		 8 bit wide words are encoded as 0x00111 =  7 (+1) =  8;
+
+        qpi: 0 = spi, 1 = qspi // use 1 bit or use quad-spi, but the MOSI lines are 3, not 4: CHECK!
+
         lsbfirst: 0 = MSB_FIRST, 1 = LSB_FIRST
   */
-  cmd->cmd[2] = SPI_CMD_TX_DATA(len/32, SPI_CMD_1_WORD_PER_TRANSF, 32, qspi, SPI_CMD_MSB_FIRST);
-  cmd->cmd[3] = SPI_CMD_EOT(1, cs_mode == RT_SPIM_CS_KEEP);
+
+  // cmd->cmd[2] = SPI_CMD_TX_DATA(len/32, SPI_CMD_1_WORD_PER_TRANSF, 32, qspi, SPI_CMD_MSB_FIRST);
+  cmd->cmd[2] = SPI_CMD_TX_DATA(
+	len/32,                    /* 32-bit words to transfer */ \
+	SPI_CMD_1_WORD_PER_TRANSF, /* how many words per transfer (1 word == '00') */ \
+	32,                        /* size of MOSI word: 8 is stored as %00111 = 7, 32 as %11111 = 31 */ \
+	qspi,                      /* '0' == 1-bit spi; '1' == 3-4 bits qspi */ \
+	SPI_CMD_MSB_FIRST          /* first MOSI word bit to send: 0 = MSB_FIRST, 1 = LSB_FIRST */ \
+	);
+
+  cmd->cmd[3] = SPI_CMD_EOT(1, cs_mode == RT_SPIM_CS_KEEP); // what is this command doing with CS?! CHECK!
 
   if (likely(__rt_spim_periph_push(periph, copy)))
   {
-    int cfg = UDMA_CHANNEL_CFG_SIZE_32 | UDMA_CHANNEL_CFG_EN;
-    plp_udma_enqueue(cmd_base, (int)cmd, 4*4, cfg);
-    plp_udma_enqueue(channel_base, (int)data, buffer_size, cfg);
+    int cfg = UDMA_CHANNEL_CFG_SIZE_32 | UDMA_CHANNEL_CFG_EN; // 'cfg' = 32-bit + enable, same for cmd microcode and buffer data DMAs
+    plp_udma_enqueue(cmd_base, (int)cmd, 4*4, cfg); // spi base, microcode commands array, size of array in bytes, 'cfg'.
+    plp_udma_enqueue(channel_base, (int)data, buffer_size, cfg); // channel_base: tx channel registers; data: pointer to tx buffer.
+								 // buffer_size: lenght of data to transfer in bytes, converted from bits.
   }
-  else
+  else // to be retrieved and submitted later?
   {
-    copy->u.raw.val[1] = periph_base;
-    copy->u.raw.val[2] = (int)cmd;
-    copy->u.raw.val[3] = 4*4;
-    copy->u.raw.val[4] = buffer_size;
-    copy->u.raw.val[5] = 0;
-    copy->u.raw.val[6] = (int)data;
+    copy->u.raw.val[1] = periph_base;	// the SPI registers
+    copy->u.raw.val[2] = (int)cmd;	// where the microcode array is
+    copy->u.raw.val[3] = 4*4;		// size of SPI cmd array of microcode: 4x 4-bytes words
+    copy->u.raw.val[4] = buffer_size;	// how many 8-bit bytes of data shall SPI send
+    copy->u.raw.val[5] = 0;		// what is this 0 for? is the CS pin #?
+    copy->u.raw.val[6] = (int)data;	// pointer to data to send
   }
 
   rt_irq_restore(irq);
@@ -376,7 +399,7 @@ void rt_spim_transfer(rt_spim_t *handle, void *tx_data, void *rx_data, size_t le
 
 void rt_spim_conf_init(rt_spim_conf_t *conf)
 {
-  conf->wordsize = RT_SPIM_WORDSIZE_8;
+  conf->wordsize = RT_SPIM_WORDSIZE_8; // note: WORDSIZE_8? and how is this used for send/receive?
   conf->big_endian = 0;
   conf->max_baudrate = 10000000;
   conf->cs_gpio = -1;
